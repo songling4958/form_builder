3197d4396475aea155fed98dbfc251a27f0e6d5d
songling4958 <songling19960501@gmail.com>
2024-01-14 15:40:56 +0100

    initial page layout

---
+++ actions/form.ts
@@ -0,0 +1,35 @@
+import prisma from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs";
+
+class UserNotFoundErr extends Error {}
+
+export async function GetFormStats() {
+  const user = await currentUser();
+  if (!user) {
+    throw new UserNotFoundErr();
+  }
+
+  const stats = await prisma.form.aggregate({
+    where: {
+      userId: user.id,
+    },
+    _sum: {
+      visits: true,
+      submissions: true,
+    },
+  });
+
+  const visits = stats._sum.visits || 0;
+  const submissions = stats._sum.submissions || 0;
+
+  const submissionRate = visits > 0 ? (submissions / visits) * 100 : 0;
+
+  const bounceRate = 100 - submissionRate;
+
+  return {
+    visits,
+    submissions,
+    submissionRate,
+    bounceRate,
+  };
+}
