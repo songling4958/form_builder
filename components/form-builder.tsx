0a053eeae20c0f69a31115d89186c734bad5e77d
songling4958 <songling19960501@gmail.com>
2024-01-19 13:43:19 +0100

    update designer to handle DnD

--- components/form-builder.tsx
+++ components/form-builder.tsx
@@ -1,32 +1,73 @@
+"use client";
+
 import { Form } from "@prisma/client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PreviewDialogBtn from "./preview-dialog-btn";
 import SaveFormBtn from "./save-form-btn";
 import PublishFormBtn from "./publish-form-btn";
 import Designer from "./designer";
+import {
+  DndContext,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import DragOverlayWrapper from "./drag-overlay-wrapper";
+import useDesigner from "./hooks/use-designer";
 
 const FormBuilder = ({ form }: { form: Form }) => {
+  const { setElements, setSelectedElement } = useDesigner();
+  const [isReady, setIsReady] = useState(false);
+
+  const mouseSensor = useSensor(MouseSensor, {
+    activationConstraint: {
+      distance: 10, // 10px
+    },
+  });
+
+  const touchSensor = useSensor(TouchSensor, {
+    activationConstraint: {
+      delay: 300,
+      tolerance: 5,
+    },
+  });
+
+  const sensors = useSensors(mouseSensor, touchSensor);
+
+  useEffect(() => {
+    if (isReady) return;
+    const elements = JSON.parse(form.content);
+    setElements(elements);
+    setSelectedElement(null);
+    const readyTimeout = setTimeout(() => setIsReady(true), 500);
+    return () => clearTimeout(readyTimeout);
+  }, [form, setElements, isReady, setSelectedElement]);
+
   return (
-    <main className="flex flex-col w-full">
-      <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
-        <h2 className="truncate font-medium">
-          <span className="text-muted-foreground mr-2">Form:</span>
-          {form.name}
-        </h2>
-        <div className="flex items-center gap-2">
-          <PreviewDialogBtn />
-          {!form.published && (
-            <>
-              <SaveFormBtn id={form.id} />
-              <PublishFormBtn id={form.id} />
-            </>
-          )}
+    <DndContext sensors={sensors}>
+      <main className="flex flex-col w-full">
+        <nav className="flex justify-between border-b-2 p-4 gap-3 items-center">
+          <h2 className="truncate font-medium">
+            <span className="text-muted-foreground mr-2">Form:</span>
+            {form.name}
+          </h2>
+          <div className="flex items-center gap-2">
+            <PreviewDialogBtn />
+            {!form.published && (
+              <>
+                <SaveFormBtn id={form.id} />
+                <PublishFormBtn id={form.id} />
+              </>
+            )}
+          </div>
+        </nav>
+        <div className="flex w-full flex-grow items-center justify-center relative overflow-y-auto h-[200px] bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]">
+          <Designer />
         </div>
-      </nav>
-      <div className="flex w-full flex-grow items-center justify-center relative overflow-y-auto h-[200px] bg-accent bg-[url(/paper.svg)] dark:bg-[url(/paper-dark.svg)]">
-        <Designer />
-      </div>
-    </main>
+      </main>
+      <DragOverlayWrapper />
+    </DndContext>
   );
 };
 
